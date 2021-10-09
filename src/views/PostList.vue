93ad99b27e627a3dbfbe38d9f3d7b7661783dc0d
Ywoosang <opellong13@gmail.com>
2021-10-09 21:06:38 +0900

    Add tablet category title size #15

--- src/views/PostList.vue
+++ src/views/PostList.vue
@@ -206,6 +206,9 @@ export default {
 
 /* 테블릿 가로, 테블릿 세로 (해상도 768px ~ 1023px)*/
 @media all and (min-width: 768px) and (max-width: 1023px) {
+  .title h1{
+    font-size: 2.5rem;
+  }
 }
 /* 모바일 가로, 모바일 세로 (해상도 480px ~ 767px)*/
 @media all and (max-width: 767px) {
